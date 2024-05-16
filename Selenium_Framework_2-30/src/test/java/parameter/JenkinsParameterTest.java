1c95a099d4c768c4243315557f0a8e91c077862b
AbhijithTyss <abhijith.styss@gmail.com>
2024-05-16 16:23:00 +0530

    added JenkinsParameterTest class

---
+++ Selenium_Framework_2-30/src/test/java/parameter/JenkinsParameterTest.java
@@ -0,0 +1,11 @@
+package parameter;
+
+import org.testng.annotations.Test;
+
+public class JenkinsParameterTest {
+	@Test
+	public void recieveDataFromJenkins() {
+		String browserValue = System.getProperty("browser");
+		System.out.println(browserValue);
+	}
+}
